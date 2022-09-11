87c44adce7800b5e865257872f4aa5612a5dbad8
uzanenis <uzanenis@outlook.com>
2022-09-11 22:20:08 +0300

    homepage and blogs pages separeted

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Header from "../components/header"
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="antialiased">
+      <Header />
+      <main className="mt-6 mb-20">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
 
 export default MyApp
---
+++ pages/blog.js
@@ -0,0 +1,38 @@
+import Head from "next/head";
+import Link from "next/link";
+import ReactMarkdown from 'react-markdown'
+
+
+export default function BlogPage({ posts }) {
+  return (
+    <div className="site-container">
+      
+      
+      {posts.map((post) => (
+        <div className="blog" key={post.slug}>
+        <h2 className="blog-title">
+          <Link href={post.slug}>
+            <a className="blog-title-link">{post.title}</a>
+          </Link>
+        </h2>
+        <div className="blog-text">
+          <ReactMarkdown>{post.details}</ReactMarkdown>
+        </div>
+        <div className="blog-date">{post.date}</div>
+      </div>
+      ))}
+
+      <footer></footer>
+      <style jsx>{``}</style>
+    </div>
+  );
+
+  
+}
+
+BlogPage.getInitialProps = async ({query}) => {
+  //const res = await fetch('http://enisuzan-dev.vercel.app/api/posts')
+  const res = await fetch('http://localhost:3000/api/posts')
+  const json = await res.json()
+  return { posts: json.posts }
+}
